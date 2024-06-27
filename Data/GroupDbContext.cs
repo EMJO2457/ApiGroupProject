e4faa5dc0954a5d6b27b67cba79737760c6a209b
Demetri Warren <dwarren@tql.com>
2024-06-27 12:18:50 -0400

    added using statements to Item Class

---
+++ Data/GroupDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiGroupProject.Models;
+
+namespace ApiGroupProject.Data
+{
+    public class GroupDbContext : DbContext
+    {
+        public GroupDbContext (DbContextOptions<GroupDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiGroupProject.Models.Employee> Employee { get; set; } = default!;
+    }
+}
