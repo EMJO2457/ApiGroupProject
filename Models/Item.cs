18ca51228f84bc89326f32dc53ad77c76a8fdb55
Eric Jones <emjones@tql.com>
2024-06-27 11:25:27 -0400

    Updating item

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,6 +1,15 @@
-﻿namespace ApiGroupProject.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiGroupProject.Models
 {
     public class Item
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; }=string.Empty;
+
+        [Column(TypeName = "decimal(9,2)")]
+        public decimal price { get; set; }
     }
 }
