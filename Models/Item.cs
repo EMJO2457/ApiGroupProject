a7773bbdf528927d8f709d6ff9fcda16c49be615
James Luncan <jluncan@tql.com>
2024-06-27 11:07:21 -0400

    item class

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,6 +1,21 @@
-﻿namespace ApiGroupProject.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiGroupProject.Models
 {
     public class Item
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(9,2)")]
+        public decimal price { get; set; }
+
+
+
+
+
+
     }
 }
